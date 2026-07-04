b9ed9017b2a306a5bb28cf4ab0e2f80789e878fa
tp <theotime.perras@gmail.com>
2026-07-04 02:15:06 -0400

    Added every server message. Some are commented out to get this to compile as it is, and no new requests are tested.

--- src/main.zig
+++ src/main.zig
@@ -73,6 +73,11 @@ const UserStats = struct {
     dirs: u32,
 };
 
+const Recommendation = struct {
+    recommendation: []u8,
+    recommendations: i32
+};
+
 pub const messages = struct {
     pub const Login = struct {
         pub const code = 1;
@@ -278,8 +283,8 @@ pub const messages = struct {
     pub const MessageUser = struct {
         pub const code = 22;
 
-        username: []u8,
-        message: []u8,
+        username: []const u8,
+        message: []const u8,
 
         pub const Response = struct {
             username: []u8,
@@ -339,9 +344,501 @@ pub const messages = struct {
         pub const Response = void;
     };
 
-
-    pub const UserSearch = struct { 
+    pub const UserSearch = struct {
         pub const code = 42;
+    };
+
+    pub const AddThingILike = struct {
+        pub const code = 51;
+
+        item: []const u8,
+    };
+
+    pub const RemoveThingILike = struct {
+        pub const code = 52;
+
+        item: []const u8,
+    };
+
+    // TODO
+    // pub const Recommendations = struct {
+    //     pub const code = 54;
+
+    //     pub const Response = struct {
+    //         recommendations: []Recommendation,
+    //         unrecommendations: []Recommendation,
+    //     };
+    // };
+
+    // pub const GlobalRecommendations = struct {
+    //     pub const code = 56;
+    //     pub const Response = Recommendations.Response;
+    // };
+
+    // TODO:
+    // pub const UserInterests = struct {
+    //     pub const code = 57;
+
+    //     username: []const u8,
+
+    //     pub const Response = struct {
+    //         username: []u8,
+    //         liked: [][]u8,
+    //         hated: [][]u8,
+    //     };
+    // };
+
+    // TODO:
+    // pub const RoomList = struct {
+    //     pub const code = 64;
+
+    //     pub const Response = struct {
+    //         const List = struct {
+    //             names: [][]u8,
+    //             users: [][]u32,
+    //         };
+
+    //         public: List,
+    //         private: List,
+    //         abandoned: List,
+    //     };
+    // };
+
+    pub const AdminMessage = struct {
+        pub const code = 66;
+
+        pub const Response = struct {
+            message: []u8
+        };
+    };
+
+    // TODO:
+    // pub const PrivilegedUsers = struct {
+    //     pub const code = 69;
+
+    //     pub const Response = struct {
+    //         users: [][]u8
+    //     };
+    // };
+
+    pub const HaveNoParent = struct {
+        pub const code = 71;
+
+        no_parent: Bool
+    };
+
+    pub const ParentIp = struct {
+        pub const code = 73;
+
+        ip: u32
+    };
+
+    pub const ParentMinSpeed = struct {
+        pub const code = 83;
+
+        pub const Response = struct {
+            speed: u32
+        };
+    };
+
+    pub const ParentSpeedRatio = struct {
+        pub const code = 84;
+
+        pub const Response = struct {
+            ratio: u32
+        };
+    };
+
+    pub const CheckPrivileges = struct {
+        pub const code = 92;
+
+        pub const Response = struct {
+            seconds: u32
+        };
+    };
+
+    // TODO:
+    // pub const EmbeddedMessage = struct {
+    //     pub const code = 93;
+
+    //     pub const Response = struct {
+    //         message: main.Response,
+    //     };
+    // };
+
+    pub const AcceptChildren = struct {
+        pub const code = 100;
+
+        accept: Bool
+    };
+
+    // TODO:
+    // pub const PossibleParents = struct {
+    //     pub const code = 102;
+
+    //     pub const Response = struct {
+    //         parents: []struct {
+    //             username: []u8,
+    //             ip: u32,
+    //             port: u32
+    //         }
+    //     };
+    // };
+
+    pub const WishlistSearch = struct {
+        pub const code = 103;
+
+        token: u32,
+        query: []const u8,
+    };
+
+    pub const WishlistInterval = struct {
+        pub const code = 104;
+
+        pub const Response = struct {
+            interval: u32
+        };
+    };
+
+    // TODO:
+    // pub const SimilarUsers = struct {
+    //     pub const code = 110;
+
+    //     pub const Response = struct {
+    //         users: []struct {
+    //             username: []u8,
+    //             rating: u32
+    //         }
+    //     };
+    // };
+
+    // TODO:
+    // pub const ItemRecommendations = struct {
+    //     pub const code = 111;
+
+    //     item: []const u8,
+
+    //     pub const Response = struct {
+    //         item: []u8,
+    //         recommendations: []Recommendation
+    //     };
+    // };
+
+    // TODO:
+    // pub const ItemSimilarUsers = struct {
+    //     pub const code = 112;
+
+    //     item: []const u8,
+
+    //     pub const Response = struct {
+    //         item: []u8,
+    //         users: [][]u32
+    //     };
+    // };
+
+    // TODO:
+    // pub const RoomTickers = struct {
+    //     pub const code = 113;
+
+    //     pub const Response = struct {
+    //         room: []u8,
+    //         users: []struct {
+    //             username: []u8,
+    //             tickers: []u8
+    //         }
+    //     };
+    // };
+
+    pub const RoomTickerAdded = struct {
+        pub const code = 114;
+
+        pub const Response = struct {
+            room: []u8,
+            username: []u8,
+            ticker: []u8
+        };
+    };
+
+    pub const RoomTickerRemoved = struct {
+        pub const code = 115;
+
+        pub const Response = struct {
+            room: []u8,
+            ticker: []u8
+        };
+    };
+
+    pub const SetRoomTicker = struct {
+        pub const code = 116;
+
+        room: []const u8,
+        ticker: []const u8,
+    };
+
+    pub const AddThingIHate = struct {
+        pub const code = 117;
+
+        item: []const u8
+    };
+
+    pub const RemoveThingIHate = struct {
+        pub const code = 118;
+
+        item: []const u8
+    };
+
+    pub const RoomSearch = struct {
+        pub const code = 120;
+
+        room: []const u8,
+        token: u32,
+        query: []const u8,
+    };
+
+    pub const SendUploadSpeed = struct {
+        pub const code = 121;
+
+        speed: u32
+    };
+
+    pub const UserPrivileged = struct {
+        pub const code = 122;
+
+        username: []const u8,
+
+        pub const Response = struct {
+            username: []u8,
+            privileged: Bool
+        };
+    };
+
+    pub const GivePrivileges = struct {
+        pub const code = 123;
+
+        username: []const u8,
+        days: u32
+    };
+
+    pub const NotifyPrivileges = struct {
+        pub const code = 124;
+
+        token: u32,
+        username: []const u8,
+
+        pub const Response = struct {
+            token: u32,
+            username: []u8,
+        };
+    };
+
+    pub const AckNotifyPrivileges = struct {
+        pub const code = 125;
+
+        token: u32,
+    };
+
+    pub const BranchLevel = struct {
+        pub const code = 126;
+
+        level: u32
+    };
+
+    pub const BranchRoot = struct {
+        pub const code = 127;
+
+        root: []const u8
+    };
+
+    pub const ResetDistributed = struct {
+        pub const code = 130;
+    };
+
+    // TODO:
+    // pub const RoomMembers = struct {
+    //     pub const code = 133;
+
+    //     pub const Response = struct {
+    //         room: []u8,
+    //         members: [][]u8,
+    //     };
+    // };
+
+    // It is tempting to make room and username into a struct that could be
+    // used here and in other places as well, but I'm not sure how to handle
+    // the differing constness of the child slices cleanly in zig. A function
+    // that returns a type feels over the top here.
+    pub const AddRoomMember = struct {
+        pub const code = 134;
+
+        room: []const u8,
+        username: []const u8,
+
+        pub const Response = struct {
+            room: []u8,
+            username: []u8,
+        };
+    };
+
+    pub const RemoveRoomMember = struct {
+        pub const code = 135;
+
+        room: []const u8,
+        username: []const u8,
+
+        pub const Response = struct {
+            room: []u8,
+            username: []u8,
+        };
+    };
+
+    pub const CancelRoomMembership = struct {
+        pub const code = 136;
+
+        room: []const u8
+    };
+
+    pub const CancelRoomOwnership = struct {
+        pub const code = 137;
+
+        room: []const u8
+    };
+
+    pub const RoomMembershipGranted = struct {
+        pub const code = 139;
+
+        pub const Response = struct {
+            room: []u8
+        };
+    };
+
+    pub const RoomMembershipRevoked = struct {
+        pub const code = 140;
+
+        pub const Response = struct {
+            room: []u8
+        };
+    };
+
+    pub const EnableRoomInvitations = struct {
+        pub const code = 141;
+
+        enable: Bool,
+
+        pub const Response = struct {
+            enable: Bool,
+        };
+    };
+
+    pub const ChangePassword = struct {
+        pub const code = 142;
+
+        pass: []const u8,
+
+        pub const Response = struct {
+            pass: []u8
+        };
+    };
+
+    pub const AddRoomOperator = struct {
+        pub const code = 143;
+
+        room: []const u8,
+        username: []const u8,
+
+        pub const Response = struct {
+            room: []u8,
+            username: []u8,
+        };
+    };
+
+    pub const RemoveRoomOperator = struct {
+        pub const code = 144;
+
+        room: []const u8,
+        username: []const u8,
+
+        pub const Response = struct {
+            room: []u8,
+            username: []u8,
+        };
+    };
+
+    pub const RoomOperatorshipGranted = struct {
+        pub const code = 145;
+
+        pub const Response = struct {
+            room: []u8
+        };
+    };
+
+    pub const RoomOperatorshipRevoked = struct {
+        pub const code = 146;
+
+        pub const Response = struct {
+            room: []u8
+        };
+    };
+
+    // TODO:
+    // pub const RoomOperators = struct {
+    //     pub const code = 148;
+
+    //     pub const Response = struct {
+    //         room: []u8,
+    //         operators: [][]u8
+    //     };
+    // };
+
+    pub const MessageUsers = struct {
+        pub const code = 149;
+
+        // TODO:
+        users: []const []const u8,
+    };
+
+    pub const JoinGlobalRoom = struct {
+        pub const code = 150;
+    };
+
+    pub const LeaveGlobalRoom = struct {
+        pub const code = 151;
+    };
+
+    pub const GlobalRoomMessage = struct {
+        pub const code = 152;
+
+        pub const response = struct { 
+            room: []u8,
+            username: []u8,
+            message: []u8
+        };
+    };
+
+    // pub const ExcludedSearchPhrases = struct {
+    //     pub const code = 160;
+
+    //     pub const Response = struct {
+    //         phrases: [][]u8
+    //     };
+    // };
+
+    pub const CantConnectToPeer = struct {
+        pub const code = 1001;
+
+        token: u32,
+        username: []const u8,
+
+        pub const Response = struct {
+            token: u32
+        };
+    };
+
+    pub const CantCreateRoom = struct {
+        pub const code = 1003;
+
+        pub const Response = struct {
+            room: []u8
+        };
     };
 };
 
@@ -442,6 +939,7 @@ fn readT(T: type, gpa: Allocator, reader: *Reader) !T {
         // read bytes or finding out where the zig io interface guarantees
         // pointer validity. I should be checking message lenghts anyways.
         .optional => null,
+        .void => {},
         else => try reader.takeInt(T, .little),
     };
 }
